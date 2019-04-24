5da7baa335bb5b4e60d53134e439cf96e149e45f
Hugo Pratt <38809698+hupratt@users.noreply.github.com>
2019-04-24 16:20:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,21 @@
 <head>
 	<title>Parcel Sandbox</title>
 	<meta charset="UTF-8" />
-
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+
+<script src="jquery.flagstrap.js"></script>
+<link rel="stylesheet" type="text/css" href="flags.css">
 </head>
 
 <body>
+
+<form class="form-horizontal">
+	<div class="form-group">
+        <label>Select Country</label><br>
+        <div class="flagstrap" data-input-name="country"></div>
+    </div>
+</form>
 
 <div style='height:200px; background-color:red;text-align: center;' class = "ben" id="divToLoad-1"><img style="background-color:blue;" src="AromaticVigorousCockatiel-small.gif"/> </div>
 <div style='height:200px; background-color:red;text-align: center;' class = "ben" id="divToLoad-2"><img style="background-color:blue;" src="AromaticVigorousCockatiel-small.gif"/> </div>
@@ -24,11 +34,14 @@ $(document).ready(function(){
 
   function toad(){
     const src= "http://apod.nasa.gov/apod/image/9712/orionfull_jcc_big.jpg";
-    $(".ben").css("background-image", "url(" + src + ")");
+    $("#divToLoad-1").css("background-image", "url(" + src + ")");
     $(".ben > img").remove();
   };
+  
+// $.getScript("jquery.flagstrap.js")
+  setTimeout(toad,2000);
 
-setTimeout(toad,2000);
+  $('.flagstrap').flagStrap();
 
 });
 
